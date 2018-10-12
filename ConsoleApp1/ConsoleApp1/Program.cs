793dadbb195e16800ab6daa4af5863de0a2c379c
Elshad <safarov.elshad007@gmail.com>
2018-10-12 14:49:33 +0500

    added console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n: ");
+            int n;
+            while (!(int.TryParse(Console.ReadLine(), out n)) || n <= 0){
+                Console.Write("Нельзя вычислить. Введите n: ");                          
+            }
+            Console.Write("Введите m: ");
+            int m;
+            while (!(int.TryParse(Console.ReadLine(), out m)) || m <= 1) {
+                Console.Write("Нельзя вычислить. Введите m: ");
+            }
+            float k = (n++ / --m);
+            Console.WriteLine("n++/--m = {0}, n = {1}, m={2}", k, n, m);
+            Console.ReadLine();
+        }
+    }
+}
