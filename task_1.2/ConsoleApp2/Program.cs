c4983f042791fc8c38a28afbe8aee92061ee9a6c
Elshad <safarov.elshad007@gmail.com>
2018-10-19 13:42:27 +0500

    task 1.3 completed

---
+++ task_1.2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n: ");
+            decimal n;
+            while(!(decimal.TryParse(Console.ReadLine(), out n)) || n <= 0){
+                Console.Write("Нельзя вычислить. Введите n: ");
+            }
+            Console.Write("Введите m: ");
+            int m;
+            while(!(int.TryParse(Console.ReadLine(), out m)) || m <= 0){
+                Console.Write("Нельзя вычислить. Введите m: ");
+            }
+            var a = n-- > (n/m++);
+            Console.Write("n-- > n/m++; {0}; n = {1}; m = {2}", a, n, m);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ task_1.3/task_1.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n: ");
+            decimal n;
+            while (!(decimal.TryParse(Console.ReadLine(), out n)) || n <= 0)
+            {
+                Console.Write("Нельзя вычислить. Введите n: ");
+            }
+            Console.Write("Введите m: ");
+            decimal m;
+            while (!(decimal.TryParse(Console.ReadLine(), out m)) || m <= 0)
+            {
+                Console.Write("Нельзя вычислить. Введите m: ");
+            }
+            var a = m < n--;
+            Console.WriteLine("m < n--; {0}; n = {1}; m={2}", a, n, m);
+            Console.ReadLine();
+        }
+    }
+}
