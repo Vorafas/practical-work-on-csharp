f24cf2faef015e3c6f9f3dbac75fac2372a1219c
Elshad <safarov.elshad007@gmail.com>
2018-10-25 21:11:53 +0500

    task 2 completed

---
+++ pw_1/task_2/task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите x: ");
+            double x;
+            while (!(double.TryParse(Console.ReadLine(), out x))) {
+                Console.Write("Нельзя вычислить. Введите x: ");
+            }
+            Console.Write("Введите y: ");
+            double y;
+            while (!(double.TryParse(Console.ReadLine(), out y))) {
+                Console.Write("Нельзя вычислить. Введите y: ");
+            }
+            bool result;
+            if ((x * x + y * y <= 1) && ((x <= 0) || (y <= 0))) {
+                result = true;
+            }
+            else {
+                result = false;
+            }
+            Console.WriteLine("{0}; x = {1}; y = {2}", result, x, y);
+            Console.ReadLine();
+        }
+    }
+}
