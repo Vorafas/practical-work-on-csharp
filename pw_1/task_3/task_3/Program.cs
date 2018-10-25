438817dad735dd969d054e05807e78d33ffd6c84
Elshad <safarov.elshad007@gmail.com>
2018-10-25 20:51:42 +0500

    task 3 completed

---
+++ pw_1/task_3/task_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a: ");
+            double a;
+            while (!(double.TryParse(Console.ReadLine(), out a))) {
+                Console.Write("Нельзя вычислить. Введите a: ");
+            }
+            double b;
+            Console.Write("Введите b: ");
+            while (!(double.TryParse(Console.ReadLine(), out b))) {
+                Console.Write("Нельзя вычислить. Введите b: ");
+            }
+            double res = ((Math.Pow(a, 4) - (4 * Math.Pow(a, 3) * b) + (6 * Math.Pow(a, 2) * Math.Pow(b, 2)) - (4 * a * Math.Pow(b, 3)) + Math.Pow(b, 4)) - (Math.Pow(a, 4) - 4 * Math.Pow(a, 3) * b)) / ((6 * Math.Pow(a, 2) * Math.Pow(b, 2)) - (4 * a * Math.Pow(b, 3)) + Math.Pow(b, 4));
+            Console.WriteLine("result = {0}; a = {1}; b = {2};", res, a, b);
+            Console.ReadLine();
+        }
+    }
+}
