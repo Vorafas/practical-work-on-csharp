88782d0532f04b50fa0a04101cc2b6ccdd99a15a
Elshad <safarov.elshad007@gmail.com>
2018-10-26 15:11:45 +0500

    task 2.1 practical work 2

---
+++ task_2_dowhile/task_2_dowhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2_dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int max = 0;
+            int next;
+            next = Convert.ToInt32(Console.ReadLine());
+            do {
+                Console.Write("Для прекрашения ввода введите букву или 0. Введите число: ");
+                if (max < next) {
+                    max = next;
+                }
+            } while(int.TryParse(Console.ReadLine(), out next) && next >= 0);
+            Console.Write("Максимальный элемент в последовательности = {0}", max);
+            Console.ReadLine();
+        }
+    }
+}
