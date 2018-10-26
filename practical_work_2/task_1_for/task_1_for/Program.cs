2d7561fa0c8a1bba2ee4647e5fcece1703941ab8
Elshad <safarov.elshad007@gmail.com>
2018-10-26 14:24:37 +0500

    task 1.3 practical work 2

---
+++ practical_work_2/task_1_for/task_1_for/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int summa = 0;
+            int next;
+            for (next = 0; int.TryParse(Console.ReadLine(), out next);) {
+                Console.Write("Для прекрашения ввода введите букву. Введите число: ");
+                if ((next % 2) != 0) {
+                    summa += next;
+                }
+            }
+            Console.Write("Сумма нечётных чисел = {0}", summa);
+            Console.ReadLine();
+        }
+    }
+}
