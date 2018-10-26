7a249076c9f59c434190b2560967d15fa5c82dbc
Elshad <safarov.elshad007@gmail.com>
2018-10-26 15:04:20 +0500

    task 2 practical work 2

---
+++ task_2/task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int max = 0;
+            int next;
+            while ((int.TryParse(Console.ReadLine(), out next)) && next >= 0) {
+                Console.Write("Для прекрашения ввода введите букву или 0. Введите число: ");
+                if (max < next)
+                {
+                    max = next;
+                }
+            }
+            Console.Write("Максимальный элемент в последовательности = {0}", max);
+            Console.ReadLine();
+        }
+    }
+}
