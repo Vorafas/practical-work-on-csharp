808a976d73a067e80eb5421ae330079d8c316897
Elshad <safarov.elshad007@gmail.com>
2018-10-26 14:01:52 +0500

    task 1.2 practical work 2

---
+++ practical_work_2/task_1_dowhile/task_1_dowhile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1_dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int next = 0;
+            int summa = 0;
+            next = Convert.ToInt32(Console.ReadLine());
+            do {
+                Console.Write("Для прекрашения ввода введите букву. Введите число: ");
+                if ((next % 2) != 0) {
+                    summa += next;
+                }
+            } while (int.TryParse(Console.ReadLine(), out next));
+            Console.Write("Сумма нечётных чисел = {0}", summa);
+            Console.ReadLine();
+        }
+    }
+}
