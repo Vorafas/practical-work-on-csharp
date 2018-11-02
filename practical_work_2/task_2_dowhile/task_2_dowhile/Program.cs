c2181b02934fb1ab98b135a6729a3e320d711aff
Elshad <safarov.elshad007@gmail.com>
2018-11-02 11:24:53 +0500

    rework task 2.2 pw2

--- practical_work_2/task_2_dowhile/task_2_dowhile/Program.cs
+++ practical_work_2/task_2_dowhile/task_2_dowhile/Program.cs
@@ -10,16 +10,22 @@ namespace task_2_dowhile
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите число: ");
-            int max = 0;
-            int next;
-            next = Convert.ToInt32(Console.ReadLine());
-            do {
-                Console.Write("Для прекрашения ввода введите букву или 0. Введите число: ");
-                if (max < next) {
-                    max = next;
+            int i = 0,
+                max = 0,
+                n = 10,
+                k;
+            do{
+                Console.Write($"Введите {i + 1}-й элемент: ");
+                while(!(int.TryParse(Console.ReadLine(), out k))){
+                    Console.Write($"Введите {i + 1}-й элемент: ");
                 }
-            } while(int.TryParse(Console.ReadLine(), out next) && next > 0);
+                if(k == 0)
+                    break;
+                if(max < k){
+                    max = k;
+                }
+                i++;
+            }while(i < n);
             Console.Write("Максимальный элемент в последовательности = {0}", max);
             Console.ReadLine();
         }
