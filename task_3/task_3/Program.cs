8120f3d126ee91ef3c76a7de78b2eaabff45844e
Elshad <safarov.elshad007@gmail.com>
2018-11-02 14:36:01 +0500

    task 3 pw2

--- task_3/task_3/Program.cs
+++ task_3/task_3/Program.cs
@@ -10,16 +10,57 @@ namespace task_3
     {
         static void Main(string[] args)
         {
-            int p = (2 - 1);
-            int x = 32;
+            int max = 32;
             int k = 1;
-            for (int i = 2; i <= x; i = i * 2) {
-                k = k + i;
-                p *= (2 - k);
-                Console.WriteLine($"{i}");
+            int t = 2;
+            int x;
+            Console.Write("Введите x: ");
+            while (!(int.TryParse(Console.ReadLine(), out x)) || Ok(x) || Ok2(x))
+            {
+                Console.Write("Нельзя вычислить. Введите x: ");
             }
-            Console.Write($"{p}");
+            int a = (x - 1);
+            int b = (x - 2);
+            for (int i = 2; i <= max; i = i * 2) {
+                k = k + i;
+                a *= (x - k);
+            }
+            for (int y = 2; y <= max; y = y * 2)
+            {
+                t = t + y;
+                b *= (x - t);
+            }
+            int p = a / b;
+            Console.Write($"P = {p}; a = {a}; b = {b}");
             Console.ReadLine();
+        }
+        public static bool Ok(int x)
+        {
+            bool ok = false;
+            int s = 0;
+            for (int i = 2; i <= 64; i = i * 2)
+            {
+                s = i - 1;
+                if (x == s)
+                {
+                    ok = true;
+                    break;
+                }
+            }
+            return ok;
+        }
+        public static bool Ok2(int x)
+        {
+            bool ok = false;
+            for (int i = 2; i <= 64; i = i * 2)
+            {
+                if (x == i)
+                {
+                    ok = true;
+                    break;
+                }
+            }
+            return ok;
         }
     }
 }
