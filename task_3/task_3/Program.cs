7af0176f0f01ad0b997f8ec8a4989853a462d82f
Elshad <safarov.elshad007@gmail.com>
2018-11-02 12:15:26 +0500

    task 3 pw 2

---
+++ task_3/task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = (2 - 1);
+            int x = 32;
+            int k = 1;
+            for (int i = 2; i <= x; i = i * 2) {
+                k = k + i;
+                p *= (2 - k);
+                Console.WriteLine($"{i}");
+            }
+            Console.Write($"{p}");
+            Console.ReadLine();
+        }
+    }
+}
