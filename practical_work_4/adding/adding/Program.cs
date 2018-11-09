506d046595b2065ef552e079913cb68868b7963c
Elshad <safarov.elshad007@gmail.com>
2018-11-09 11:47:12 +0500

    Task 2 practical work 4

---
+++ practical_work_4/adding/adding/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число n: ");
+            int n;
+            while(!(int.TryParse(Console.ReadLine(), out n)) || n <= 0){
+                Console.Write("Введите число n: ");
+            }
+            int [] numbers = new int[n];
+            Random rnd = new Random();
+            for(int i = 0; i < n; i++){
+                numbers[i] = rnd.Next(1, 100);
+                Console.WriteLine($"n[{i}] = {numbers[i]}");
+            }
+            Console.Write("Введите число :");
+            int a;
+            while(!(int.TryParse(Console.ReadLine(), out a)) || n <=0){
+                Console.Write("Введите число :");
+            }
+            var numbersList = numbers.ToList();
+            for(int i = n; i < (n + a); i++){
+                numbersList.Insert(i, rnd.Next(0, 100));
+            }
+            var numbersArr = numbersList.ToArray();
+            for(var i = 0; i < numbersArr.Length; i++){
+                Console.WriteLine($"n[{i}] = {numbersArr[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
