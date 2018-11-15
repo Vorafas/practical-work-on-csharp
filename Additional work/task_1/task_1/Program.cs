9fd3e4e5ca1abb9f7de2ae9ab13c0f66eb48d420
Elshad <safarov.elshad007@gmail.com>
2018-11-15 21:37:56 +0500

    Task 1 additional work

---
+++ Additional work/task_1/task_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Массив из 20-ти элементов: ");
+            Random rnd = new Random();
+            int[] numbers = new int[20];
+            int t;
+            numbers[1] = rnd.Next(0, 20);
+            for (int i = 1; i < numbers.Length; i++) {
+                while (true) {
+                    t = rnd.Next(0, 20);
+                    if (!Check(numbers, t)) {
+                        break;
+                    }
+                }
+                numbers[i] = t;
+            }
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine($"n[{i}] = {numbers[i]}");
+            Console.ReadLine();
+        }
+        static bool Check(int[] array, int value){
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] == value) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
