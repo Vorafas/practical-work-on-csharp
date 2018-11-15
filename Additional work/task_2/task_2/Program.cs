5f10eef696192360bd358bd5ae7062f95c686894
Elshad <safarov.elshad007@gmail.com>
2018-11-15 22:19:47 +0500

    Task 2 additional work

---
+++ Additional work/task_2/task_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива: ");
+            int n;
+            int cnt = 0;
+            while (!(int.TryParse(Console.ReadLine(), out n)) || n <= 0) {
+                Console.Write("Введите количество элементов массива: ");
+            }
+            Random rnd = new Random();
+            int[] numbers = new int[n];
+            for (int i = 0; i < numbers.Length; i++) {
+                numbers[i] = rnd.Next(-10, 10);
+                Console.WriteLine($"n[{i}] = {numbers[i]}");
+            }
+            for (int i = 1; i < numbers.Length; i++) {
+                if (numbers[i] >= 0) {
+                    --i;
+                    if (numbers[i] < 0) {
+                        cnt += 1;
+                    }
+                    ++i;
+                }
+                if (numbers[i] < 0)
+                {
+                    --i;
+                    if (numbers[i] >= 0)
+                    {
+                        cnt += 1;
+                    }
+                    ++i;
+                }
+            }
+            Console.Write($"Знак менялся: {cnt}");
+            Console.ReadLine();
+        }
+    }
+}
