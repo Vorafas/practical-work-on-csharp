a589363d974c41f57511dec3b275ce0d00006d9b
Elshad Safarov <40852788+Elshad98@users.noreply.github.com>
2019-04-05 21:18:17 +0500

    Added task 2 practical work 7

---
+++ practical_work_7/mainTask/task_1/task_1/Form2.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace task_1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.массивToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.одномерныйМассивToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.двумерныйМассивToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рванныйМассивToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.массивToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnSave,
+            this.закрытьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(132, 22);
+            this.btnSave.Text = "Сохранить";
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.закрытьToolStripMenuItem_Click);
+            // 
+            // массивToolStripMenuItem
+            // 
+            this.массивToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.одномерныйМассивToolStripMenuItem,
+            this.двумерныйМассивToolStripMenuItem,
+            this.рванныйМассивToolStripMenuItem});
+            this.массивToolStripMenuItem.Name = "массивToolStripMenuItem";
+            this.массивToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.массивToolStripMenuItem.Text = "Массив";
+            // 
+            // одномерныйМассивToolStripMenuItem
+            // 
+            this.одномерныйМассивToolStripMenuItem.Name = "одномерныйМассивToolStripMenuItem";
+            this.одномерныйМассивToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.одномерныйМассивToolStripMenuItem.Text = "Одномерный массив";
+            this.одномерныйМассивToolStripMenuItem.Click += new System.EventHandler(this.одномерныйМассивToolStripMenuItem_Click);
+            // 
+            // двумерныйМассивToolStripMenuItem
+            // 
+            this.двумерныйМассивToolStripMenuItem.Name = "двумерныйМассивToolStripMenuItem";
+            this.двумерныйМассивToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.двумерныйМассивToolStripMenuItem.Text = "Двумерный массив";
+            this.двумерныйМассивToolStripMenuItem.Click += new System.EventHandler(this.двумерныйМассивToolStripMenuItem_Click);
+            // 
+            // рванныйМассивToolStripMenuItem
+            // 
+            this.рванныйМассивToolStripMenuItem.Name = "рванныйМассивToolStripMenuItem";
+            this.рванныйМассивToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.рванныйМассивToolStripMenuItem.Text = "Рванный массив";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите количество строк";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ввести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите количество столбцов";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(15, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(272, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(294, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Ввести";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(16, 271);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(353, 178);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(219, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Количество добавляемых строк";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(294, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Ввести";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(13, 191);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(274, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(13, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Исходный массив";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.White;
+            this.textBox5.Location = new System.Drawing.Point(398, 271);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox5.Size = new System.Drawing.Size(356, 178);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(398, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(158, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Обработанный массив";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Form2";
+            this.Text = "Двумерный массив";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btnSave;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem массивToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem одномерныйМассивToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem двумерныйМассивToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рванныйМассивToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ practical_work_7/mainTask/task_1/task_1/Form2.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_1
+{
+    public partial class Form2 : Form
+    {
+        int strings, columns, newStrings;
+        int[,] array;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void одномерныйМассивToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 newForm = new Form1();
+            newForm.Show();
+        }
+
+        private void двумерныйМассивToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 newForm = new Form2();
+            newForm.Show();
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                GetNumber();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetNumber();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetColumn();
+        }
+
+        private void GetColumn()
+        {
+            if (!(int.TryParse(textBox2.Text, out columns)) || Convert.ToInt32(columns) <= 0)
+            {
+                DialogResult dr = MessageBox.Show("Введите корректное число!",
+                      "Предупреждение", MessageBoxButtons.OK);
+                textBox2.Clear();
+            }
+            if (columns > 0)
+            {
+                int[,] table = new int[strings, columns];
+                textBox2.Text = $"Количество столбцов: {columns}";
+                textBox2.Enabled = false;
+                button2.Enabled = false;
+                textBox4.Enabled = true;
+                button3.Enabled = true;
+                textBox4.Focus();
+                GetArray(strings, columns, textBox3);
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                GetColumn();
+            }
+        }
+
+        private int[,] GetArray(int strings, int columns, TextBox element) {
+            array = new int[strings, columns];
+            Random rnd = new Random();
+            for (int i = 0; i < strings; i++) {
+                for (int j = 0; j < columns; j++) {
+                    array[i, j] = rnd.Next(10, 99);
+                    element.Text += $"{array[i, j]}      ";
+                }
+                element.Text += Environment.NewLine;
+            }
+            return array;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            GetNewSrings();
+        }
+
+        private void GetNewSrings()
+        {
+            if (!(int.TryParse(textBox4.Text, out newStrings)) || Convert.ToInt32(newStrings) <= 0)
+            {
+                DialogResult dr = MessageBox.Show("Введите корректное число!",
+                      "Предупреждение", MessageBoxButtons.OK);
+                textBox4.Clear();
+            }
+            if (newStrings > 0)
+            {
+                int str = newStrings + strings;
+                int[,] newArr = new int[str, columns];
+                textBox4.Text = $"Количество строк: {strings}";
+                textBox4.Enabled = false;
+                button3.Enabled = false;
+                NewArray(array, newArr);
+            }
+        }
+
+        private void textBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                GetNewSrings();
+            }
+        }
+
+        private void NewArray(int[,] array, int[,] newArr) {
+            Random rnd = new Random();
+            for (int i = 0; i < newArr.GetLength(0); i++) {
+                for (int j = 0; j < newArr.GetLength(1); j++) {
+                    if (i < array.GetLength(0))
+                    {
+                        newArr[i, j] = array[i, j];
+                        textBox5.Text += $"{newArr[i, j]}      ";
+                    }
+                    else {
+                        newArr[i, j] = rnd.Next(10, 99);
+                        textBox5.Text += $"{newArr[i, j]}      ";
+                    }
+                }
+                textBox5.Text += Environment.NewLine;
+            }
+        }
+
+        private void GetNumber()
+        {
+            if (!(int.TryParse(textBox1.Text, out strings)) || Convert.ToInt32(strings) <= 0)
+            {
+                DialogResult dr = MessageBox.Show("Введите корректное число!",
+                      "Предупреждение", MessageBoxButtons.OK);
+                textBox1.Clear();
+            }
+            if (strings > 0)
+            {
+                textBox1.Text = $"Количество строк: {strings}";
+                textBox1.Enabled = false;
+                button1.Enabled = false;
+                textBox2.Enabled = true;
+                button2.Enabled = true;
+                textBox2.Focus();
+            }
+        }
+    }
+}
